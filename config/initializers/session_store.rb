37142ac815d6fb96c15a9f67649589c7bb863fe6
Josh Silverman <joshs.silverman@gmail.com>
2014-08-12 18:19:30 -0400

    core + cookie sharing

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_ror_session'
+Rails.application.config.session_store :cookie_store, key: '_ror_session', domain: '.dev.localhost'
+# Quizmemanager::Application.config.session_store :cookie_store, key: '_quizmemanager_session', domain: :all
