747968db90ea8db4824d4353dde571c1d5affc16
Josh Silverman <joshs.silverman@gmail.com>
2014-08-13 08:12:10 -0400

    add cors middleware before warden

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module Ror
 
     # config.autoload_paths << Rails.root.join('lib')
 
-    config.middleware.use Rack::Cors do
+    config.middleware.insert 0, Rack::Cors do
       allow do
         origins '*'
         resource '*', :headers => :any, :methods => [:get, :post, :options]
